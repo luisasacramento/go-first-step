b1eac0e8b042b5f40c4a610bbde88c9ad69b5452
Luisa Santos <luisacristina.sn@icloud.com>
2023-12-05 00:03:34 -0300

    primeiros passos em Golang

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/luisasacramento/first-step-go
-
-go 1.21.4
--- main.go
+++ main.go
@@ -1,109 +1,154 @@
 package main
 
-// import "runtime/trace"
-
-//tipagem forte
-//não roda se tiver váriavel sem usar
-
-// func soma (x int, y int) int{
-// 	return x + y
-// }
-
-// func soma (x int, y int) (int,bool){
-// 	if x> 10 {
-// 		return x + y, true
-// 	}
-// 	return x + y, false
-
-// }
-
-// func main ()  {
-
-// 	//a:= "Hello World" declara e atribui a váriavel
-
-// 	// println(soma(1,2))
-// 	resultado, status := soma(10,20)
-// }
-
 import (
-	"encoding/json"
-	"fmt"      // "fmt" formatação
-	"net/http" // servidor web
+	"bufio"
+	"fmt" //impressão e formatação de msg
+	"io"
+	"net/http" // requisões web
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
-//Struct
-//C maiunculo Púbico
-// c minusculo privado
+const monitoramentos = 3 //constantes não podem ser alteradas
+const delay = 5
 
-type Course struct{
-	Name          string `json:"course"`;
-	Description   string `json:"description"`;
-	Price         int    `json:"price"`
-}
-
-//metodo
-
-func (c Course) GetFullInfo() string{
-	return fmt.Sprintf("Name: %s, Description: %s, Price: %d", c.Name, c.Description, c.Price)
-}
-
-func counter() {
-	for i :=0; i <10; i++{
-		fmt.Println(i)
-		time.Sleep(time.Second)
-	}
-}
-
-
-func worker(workerId int, data chan int){
-
-	for x:= range data {
-		fmt.Printf("Worker %d received %d\n ", workerId, x)
-		time.Sleep(time.Second)
-	}
-}
-
-//go routine - cria uma thread gerenciada pelo runtime da go 
-//cada thread 200kb
-
-// go - na frente significa uma nova thread
-//T1 - channel 
 func main() {
-	//threads
-	go counter() //T1 
-	go counter() // T2
-	counter()//T3 roda elas em paralelo(000, 111, 222 etc)
 
-	//chanel - canal de comunicação entre as threads
-	channel := make (chan int)
-	go worker(1, channel)//T2
-	go worker(2, channel)
+	exibeIntrodução()
 
-	for i := 0; i <10; i++ {
-		channel <- i
+	for {
+		exibeMenu()
+
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			imprimeLogs()
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0) //tudo correu bem
+
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1) //indica que ocorreu algum erro - 255
+		}
+
 	}
+}
 
-	//output - Worker 1 received 1
-	//output - Worker 1 received 2
-	//output - Worker 1 received 3....
-
-
-	
-	// fmt.Println(course.GetFullInfo())
-
-	// fmt.Fprintf()
-	http.HandleFunc("/", home)
-	http.ListenAndServe(":8080", nil)
+func exibeIntrodução() {
+	nome := "Douglas"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
 
 }
 
-func home(w http.ResponseWriter, r *http.Request ){
-	course := Course {
-		Name:  "Golang",
-	    Description:   "Golang Course",
-	    Price:      100,
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
+
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	// sites := []string{"https://httpbin.org/status/404", "https://httpbin.org/status/200", "https://httpbin.org/status/404"}
+
+	sites := leSitesDoArquivo()
+
+	// fmt.Println(sites)
+
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testanto site", i, ":", site)
+			testaSite(site)
+
+		}
+
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
 	}
 
-	json.NewEncoder(w).Encode(course)
+	fmt.Println("")
+
+}
+func testaSite(site string) {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		registraLog(site, true)
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		registraLog(site, false)
+	}
+
+}
+
+func leSitesDoArquivo() []string {
+
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		sites = append(sites, linha)
+		if err == io.EOF {
+			break
+		}
+	}
+
+	arquivo.Close()
+	return sites
+}
+
+func registraLog(site string, status bool) {
+
+	arquivo, err := os.OpenFile("log.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site +
+		" - online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+}
+
+func imprimeLogs(){
+
+	fmt.Println("Exibindo Logs...")
+
+	arquivo, err := os.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+	
+	fmt.Println(string(arquivo))
 }
